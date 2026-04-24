bf9d962c9376ad30eeea1a5127d7d13225ef8ca4
Koji Moriyama <hocchiki.hasami0821@gmail.com>
2026-04-25 07:08:53 +0900

    change the category title

--- app/homepage/Categories.js
+++ app/homepage/Categories.js
@@ -5,7 +5,7 @@ import CategoriesList from "./CategoriesList";
 export default function Categories() {
   return (
     <div className={styles.categories}>
-      <h2>Our Categories</h2>
+      <h2>Explore Categories</h2>
       <ErrorBoundary fallback="Could not load categories, please refresh the page.">
         <CategoriesList />
       </ErrorBoundary>
